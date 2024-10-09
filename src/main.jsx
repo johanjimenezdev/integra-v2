3dda1ccb575a6bd9ce185bf73beb1402a63fe39e
johanjimenezdev <jimeneza.johan27@gmail.com>
2024-10-09 09:06:02 -0500

    feat: firebase db integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
+import Firebase, { FirebaseContext } from './firebase'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <CssBaseline />
+      <App />
+    </FirebaseContext.Provider>
   </StrictMode>
 )
